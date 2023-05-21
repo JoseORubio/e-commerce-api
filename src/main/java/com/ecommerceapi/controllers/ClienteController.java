0977498a83bdd14bed2c3b507d3b7919705a8c79
JoseORubio <joseorj@yahoo.com.br>
2023-05-21 15:40:40 -0300

    Cancelado o dto e implantado Error

--- src/main/java/com/ecommerceapi/controllers/ClienteController.java
+++ src/main/java/com/ecommerceapi/controllers/ClienteController.java
@@ -6,9 +6,15 @@ import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -21,8 +27,19 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> salvarCliente(@RequestBody @Valid ClienteModel clienteModel){
+    public ResponseEntity<Object> salvarCliente(@RequestBody @Valid ClienteModel clienteModel, Errors errors){
+    if (errors.hasErrors()){
+        List<Map<String, String>> listaErros = new ArrayList<>();
 
+        List<FieldError> listaFieldErros = errors.getFieldErrors();
+        for (FieldError erro: listaFieldErros) {
+            Map<String, String> msgErro = new HashMap<String, String>();
+            msgErro.put("field", erro.getField());
+            msgErro.put("defaultMessage", erro.getDefaultMessage());
+            listaErros.add(msgErro);
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(listaErros);
+    }
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.salvarCliente(clienteModel));
     }
 
