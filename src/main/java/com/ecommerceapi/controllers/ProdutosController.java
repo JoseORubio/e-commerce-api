41012eaf370023daae8083d9c33640d57a2e9e8c
JoseORubio <joseorj@yahoo.com.br>
2023-05-31 09:39:33 -0300

    CRUD produtos completo

--- src/main/java/com/ecommerceapi/controllers/ProdutosController.java
+++ src/main/java/com/ecommerceapi/controllers/ProdutosController.java
@@ -23,43 +23,42 @@ public class ProdutosController {
 
     final ProdutoService produtoService;
 
-    public ProdutosController(ProdutoService produtoService){
+    public ProdutosController(ProdutoService produtoService) {
         this.produtoService = produtoService;
     }
 
     @PostMapping
-    public ResponseEntity<Object> cadastrarProduto(@RequestBody @Valid ProdutoDTO produtoDTO, BindingResult bindingResult){
+    public ResponseEntity<Object> cadastrarProduto(@RequestBody @Valid ProdutoDTO produtoDTO, BindingResult bindingResult) {
 
-        List<Map<String,String >> listaErros = new ArrayList<>();
-        if (bindingResult.hasErrors()){
+        List<Map<String, String>> listaErros = new ArrayList<>();
+        if (bindingResult.hasErrors()) {
             var listaFieldErrors = bindingResult.getFieldErrors();
-            for (FieldError erro: listaFieldErrors){
+            for (FieldError erro : listaFieldErrors) {
                 listaErros.add(ControllerUtils.adicionarErros(erro.getField(), erro.getDefaultMessage()));
             }
         }
-        ProdutoModel produtoModel = new ProdutoModel();
-        BeanUtils.copyProperties(produtoDTO,produtoModel);
 
-        if (produtoService.existsByNome(produtoModel.getNome())){
+        if (produtoService.existsByNome(produtoDTO.getNome())) {
             listaErros.add(ControllerUtils.adicionarErros("nome", "Nome já utilizado."));
         }
 
-
-
-        if (!listaErros.isEmpty()){
+        if (!listaErros.isEmpty()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(listaErros);
         }
 
+        ProdutoModel produtoModel = new ProdutoModel();
+        BeanUtils.copyProperties(produtoDTO, produtoModel);
+
         produtoModel.setQuantidade_estoque(Integer.parseInt(produtoDTO.getQuantidade_estoque()));
         produtoModel.setPreco(BigDecimal.valueOf(
-                Double.parseDouble(produtoDTO.getPreco().replace(',','.'))));
+                Double.parseDouble(produtoDTO.getPreco().replace(',', '.'))));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.salvarProduto(produtoModel));
     }
 
 
     @GetMapping
-    public ResponseEntity<List<ProdutoModel>> buscarProdutos(){
+    public ResponseEntity<List<ProdutoModel>> buscarProdutos() {
         return ResponseEntity.status(HttpStatus.OK).body(produtoService.buscarProdutos());
     }
 
@@ -80,7 +79,7 @@ public class ProdutosController {
     }
 
     @GetMapping("/pesquisar/{nome}")
-    public ResponseEntity<Object> pesquisarProdutos(@PathVariable(value = "nome") String nome){
+    public ResponseEntity<Object> pesquisarProdutos(@PathVariable(value = "nome") String nome) {
         Optional<List<ProdutoModel>> produtoOptional = produtoService.pesquisarProdutos(nome);
         if (produtoOptional.get().isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado.");
@@ -89,7 +88,7 @@ public class ProdutosController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deletarProduto(@PathVariable(value = "id") String id){
+    public ResponseEntity<Object> deletarProduto(@PathVariable(value = "id") String id) {
         UUID uuid = null;
         try {
             uuid = UUID.fromString(id);
@@ -108,7 +107,7 @@ public class ProdutosController {
 
     @PutMapping("{id}")
     public ResponseEntity<Object> atualizarProduto(@PathVariable(value = "id") String id,
-                                                   @RequestBody @Valid ProdutoDTO produtoDTO, BindingResult bindingResult){
+                                                   @RequestBody @Valid ProdutoDTO produtoDTO, BindingResult bindingResult) {
 
         UUID uuid = null;
         try {
@@ -122,30 +121,52 @@ public class ProdutosController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado.");
         }
 
-        List<Map<String,String >> listaErros = new ArrayList<>();
-        if (bindingResult.hasErrors()){
+        List<Map<String, String>> listaErros = new ArrayList<>();
+        List<String> listaNulos = new ArrayList<>();
+
+        if (bindingResult.hasErrors()) {
             var listaFieldErrors = bindingResult.getFieldErrors();
-            for (FieldError erro: listaFieldErrors){
-                listaErros.add(ControllerUtils.adicionarErros(erro.getField(), erro.getDefaultMessage()));
+            for (FieldError erro : listaFieldErrors) {
+
+                if (erro.getRejectedValue() == null) {
+                    listaNulos.add(erro.getField());
+                } else {
+                    listaErros.add(ControllerUtils.adicionarErros(erro.getField(), erro.getDefaultMessage()));
+                }
             }
         }
 
-        ProdutoModel produtoModel = new ProdutoModel();
-        BeanUtils.copyProperties(produtoDTO,produtoModel);
-        produtoModel.setId(uuid);
-
-        if (!produtoModel.getNome().equals(produtoOptional.get().getNome())
-                && produtoService.existsByNome(produtoModel.getNome())){
+        if (produtoDTO.getNome()!= null
+                && !produtoDTO.getNome().equals(produtoOptional.get().getNome())
+                && produtoService.existsByNome(produtoDTO.getNome())) {
             listaErros.add(ControllerUtils.adicionarErros("nome", "Nome já utilizado."));
         }
 
-        if (!listaErros.isEmpty()){
+        if (!listaErros.isEmpty()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(listaErros);
         }
 
-        produtoModel.setQuantidade_estoque(Integer.parseInt(produtoDTO.getQuantidade_estoque()));
+        ProdutoModel produtoModel = new ProdutoModel();
+        BeanUtils.copyProperties(produtoDTO, produtoModel);
+        produtoModel.setId(uuid);
+
+        for (String campo : listaNulos) {
+            switch (campo) {
+                case "nome" : produtoModel.setNome(produtoOptional.get().getNome()); break;
+                case "quantidade_estoque": produtoModel.setQuantidade_estoque(produtoOptional.get().getQuantidade_estoque()); break;
+                case "preco" : produtoModel.setPreco(produtoOptional.get().getPreco());break;
+            }
+//            if (campo.equals("nome")) produtoModel.setNome(produtoOptional.get().getNome());
+//            if (campo.equals("quantidade_estoque"))
+//                produtoModel.setQuantidade_estoque(produtoOptional.get().getQuantidade_estoque());
+//            if (campo.equals("preco")) produtoModel.setPreco(produtoOptional.get().getPreco());
+        }
+
+        if (produtoDTO.getQuantidade_estoque() != null)
+            produtoModel.setQuantidade_estoque(Integer.parseInt(produtoDTO.getQuantidade_estoque()));
+        if (produtoDTO.getPreco() != null)
         produtoModel.setPreco(BigDecimal.valueOf(
-                Double.parseDouble(produtoDTO.getPreco().replace(',','.'))));
+                Double.parseDouble(produtoDTO.getPreco().replace(',', '.'))));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.salvarProduto(produtoModel));
     }
