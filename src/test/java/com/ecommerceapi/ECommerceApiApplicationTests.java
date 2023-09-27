af4b0df2fff146ad39799c9b888833db97e43a9e
JoseORubio <joseorj@yahoo.com.br>
2023-09-27 10:11:28 -0300

    Habilitando testes 2

---
+++ src/test/java/com/ecommerceapi/ECommerceApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Teste");
+	}
+
+}
